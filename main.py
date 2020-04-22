f596f7a00628964994f570b3bd3b7b968d8b1e78
Leo-Dell-Win <leojhon.song@outlook.com>
2020-04-22 23:25:33 +0800

    fix(system): fix wierd bug only on Windows
    the bug output is
    RuntimeError:
    An attempt has been made to start a new process before the current
    process has finished its bootstrapping phase.

--- main.py
+++ main.py
@@ -29,29 +29,30 @@ def decide(signal_queue, command_queue):
 
 
 # program starts ###############################################################
-start = time.time()
-logoInfo()
+if __name__ == "__main__":
+    start = time.time()
+    logoInfo()
 
-# create pipes
-command_queue = multiprocessing.Queue()
-signal_queue = multiprocessing.Queue()
+    # create pipes
+    command_queue = multiprocessing.Queue()
+    signal_queue = multiprocessing.Queue()
 
-# initial processes and set them as deamon process
-controller_process = multiprocessing.Process(target=control, args=(command_queue, ))
-detector_process = multiprocessing.Process(target=detect, args=(signal_queue, ))
-decider_process = multiprocessing.Process(target=decide, args=(signal_queue, command_queue,))
-controller_process.daemon = True
-detector_process.daemon = True
-decider_process.daemon = True
-controller_process.start()
-detector_process.start()
-decider_process.start()
+    # initial processes and set them as deamon process
+    controller_process = multiprocessing.Process(target=control, args=(command_queue, ))
+    detector_process = multiprocessing.Process(target=detect, args=(signal_queue, ))
+    decider_process = multiprocessing.Process(target=decide, args=(signal_queue, command_queue,))
+    controller_process.daemon = True
+    detector_process.daemon = True
+    decider_process.daemon = True
+    controller_process.start()
+    detector_process.start()
+    decider_process.start()
 
-# when flag_patio_finished becomes True, program ends, and all processes end with it
-while True:
-    # now use time to be finish flag signal
-    if time.time() - start > 1:  # 1s
-        flag_patio_finished = True
-    if flag_patio_finished:
-        break
-info('Finished 🍻')
+    # when flag_patio_finished becomes True, program ends, and all processes end with it
+    while True:
+        # now use time to be finish flag signal
+        if time.time() - start > 1:  # 1s
+            flag_patio_finished = True
+        if flag_patio_finished:
+            break
+    info('Finished 🍻')
