5f451cb05be22031683cf12bfbdfb587df03c90e
leo-Inspiron-3568 <LeoJhon.Song@outlook.com>
2020-05-17 02:13:58 +0800

    feat(system): update install.sh for rpi environment config

--- config/README.md
+++ config/README.md
@@ -1,6 +1,8 @@
-## install.sh说明
+# install.sh说明
 
 用途说明见该脚本注释.
+
+## OpenCV
 
 - 为什么指定OpenCV版本: 默认的最新版目前还有问题, 容易出现**undefined symbol: __atomic_fetch_add8**的报错. 🔗参见[这里](https://www.pyimagesearch.com/2018/09/19/pip-install-opencv/) (需要在页面手动搜索一下这个报错)
 - 为什么是本地安装: 因为我尝试了许多次pip在线安装opencv, 但是网速基本为0, 因此我们采用**手动下载wheel文件并本地使用wheel文件安装**的方式安装opencv. 效果与`pip install opencv-contrib-python==4.1.0.25`没有区别, 因为这条命令只比本地安装多了一步下载.
@@ -18,3 +20,7 @@ python -c "import cv2; print(cv2.__version__)"
 应当打印出OpenCV的版本号`4.1.0`
 
 💡 在确认成功安装后wheel文件就可以删掉了.
+
+## wiringPi
+
+目前rpi4b源里的wiringPi版本不够新, 无法识别rpi4的gpio, 需要手动更新. 详情见[wiringPi updated to 2.52 for the Raspberry Pi 4B](http://wiringpi.com/wiringpi-updated-to-2-52-for-the-raspberry-pi-4b/)
--- config/install.sh
+++ config/install.sh
@@ -17,21 +17,33 @@ sudo sh -c 'echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bus
 sudo sh -c 'echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib" >> /etc/apt/sources.list'
 sudo sh -c 'echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui" > /etc/apt/sources.list.d/raspi.list'
 sudo apt update
+echo '\e[32mapt source changed\e[0m'
 
-# remove the soft links to pip2 and python2
-sudo rm /usr/bin/pip
-sudo rm /usr/bin/python
 # then install update-alternatives
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+echo '\e[32mpython, pip changed to python3 and pip3\e[0m'
 
 # change to tsinghua pip mirror and upgrade pip
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install pip -U
+echo '\e[32mpip source changed\e[0m'
 
 # install OpenCV and its dependencies
-sudo apt-get install -y libhdf5-dev libhdf5-103
-sudo apt-get install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
-sudo apt-get install -y libatlas-base-dev
-sudo apt-get install -y libjasper-dev
+# sudo apt-get install -y libhdf5-dev libhdf5-103
+# sudo apt-get install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+# sudo apt-get install -y libatlas-base-dev
+# sudo apt-get install -y libjasper-dev
 pip install ~/Desktop/opencv_contrib_python-4.1.0.25-cp37-cp37m-linux_armv7l.whl  # ❗this wheel file has to be there first
+echo '\e[32mopencv-contrib-python installed\e[0m'
+
+# upgrade wiringPi to 2.52
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+rm wiringpi-latest.deb
+echo '\e[32mwiringPi upgraded'
+
+# install and change shell to zsh
+sudo apt install zsh -y
+chsh -s /bin/zsh
+echo '\e[32mchanged shell to zsh\e[0m'
