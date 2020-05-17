372d3a2f375cca79211cf2299139865923e62e5a
leo-Inspiron-3568 <LeoJhon.Song@outlook.com>
2020-05-17 19:09:01 +0800

    Revert to "refactor: refactor get_color to make it more readable"

--- webots/controllers/arm_robot/build/release/arm_robot.d
+++
@@ -1,5 +0,0 @@
-build/release/arm_robot.o: arm_robot.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h
