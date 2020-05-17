2d77af1de476effb0f55def296eb6ba6ee4b0dbb
leo-Inspiron-3568 <LeoJhon.Song@outlook.com>
2020-05-18 02:16:48 +0800

    style: prettier the code

--- webots/controllers/mihotel_rover/mihotel_rover.py
+++ webots/controllers/mihotel_rover/mihotel_rover.py
@@ -99,6 +99,7 @@ if __name__ == "__main__" and flag_simulation:
         with lock:
             flag_pause.value = False
             key.value = keyboard.getKey()  # character of the key press
+    info('Finished!')
 
 # if run for real rover
 if __name__ == "__main__" and not flag_simulation:
