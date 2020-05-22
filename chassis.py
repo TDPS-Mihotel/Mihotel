0bf4037edcc289346576d81c59d11e244a33f6a0
howard-home <2429571337@qq.com>
2020-05-22 23:50:44 +0800

    modify control
    set default velocity and max velocity.
    add turn with angle command

--- chassis.py
+++ chassis.py
@@ -52,6 +52,8 @@ class Controller(object):
 
     def __init__(self):
         self.velocityDict = {}
+        self.defaultVelocity = 30
+        self.maxVelocity = 50
         info('Chassis initialed')
 
     def set_queue(self, command_queue, motors_queue):
@@ -81,33 +83,41 @@ class Controller(object):
                 self.state = 'Arm rotating'
                 self.velocityDict['arm'] = 10
             # wheel
+            if command[:4] == 'Turn' and command[:5] != 'Turn ':
+                shift = float(command[4:]) * 10
+                self.state = command
+                self.velocityDict['wheel1'] = self.defaultVelocity + shift
+                self.velocityDict['wheel2'] = self.defaultVelocity + shift
+                self.velocityDict['wheel3'] = self.defaultVelocity - shift
+                self.velocityDict['wheel4'] = self.defaultVelocity - shift
+
             if command == 'Move forward':
                 self.state = 'Moving forward'
-                self.velocityDict['wheel1'] = -10
-                self.velocityDict['wheel2'] = -10
-                self.velocityDict['wheel3'] = -10
-                self.velocityDict['wheel4'] = -10
+                self.velocityDict['wheel1'] = self.defaultVelocity
+                self.velocityDict['wheel2'] = self.defaultVelocity
+                self.velocityDict['wheel3'] = self.defaultVelocity
+                self.velocityDict['wheel4'] = self.defaultVelocity
 
             if command == 'Move backward':
                 self.state = 'Moving backward'
-                self.velocityDict['wheel1'] = 10
-                self.velocityDict['wheel2'] = 10
-                self.velocityDict['wheel3'] = 10
-                self.velocityDict['wheel4'] = 10
+                self.velocityDict['wheel1'] = -self.defaultVelocity
+                self.velocityDict['wheel2'] = -self.defaultVelocity
+                self.velocityDict['wheel3'] = -self.defaultVelocity
+                self.velocityDict['wheel4'] = -self.defaultVelocity
 
             if command == 'Turn right':
                 self.state = 'Turning right'
-                self.velocityDict['wheel1'] = -10
-                self.velocityDict['wheel2'] = -10
-                self.velocityDict['wheel3'] = 10
-                self.velocityDict['wheel4'] = 10
+                self.velocityDict['wheel1'] = self.defaultVelocity
+                self.velocityDict['wheel2'] = self.defaultVelocity
+                self.velocityDict['wheel3'] = -self.defaultVelocity
+                self.velocityDict['wheel4'] = -self.defaultVelocity
 
             if command == 'Turn left':
                 self.state = 'Turning left'
-                self.velocityDict['wheel1'] = 10
-                self.velocityDict['wheel2'] = 10
-                self.velocityDict['wheel3'] = -10
-                self.velocityDict['wheel4'] = -10
+                self.velocityDict['wheel1'] = -self.defaultVelocity
+                self.velocityDict['wheel2'] = -self.defaultVelocity
+                self.velocityDict['wheel3'] = self.defaultVelocity
+                self.velocityDict['wheel4'] = self.defaultVelocity
 
             if command == 'Stop':
                 self.state = 'Stopped'
@@ -115,6 +125,13 @@ class Controller(object):
                 self.velocityDict['wheel2'] = 0
                 self.velocityDict['wheel3'] = 0
                 self.velocityDict['wheel4'] = 0
+
+            for item in self.velocityDict:
+                if self.velocityDict[item] < -self.maxVelocity:
+                    self.velocityDict[item] = -self.maxVelocity
+                if self.velocityDict[item] > self.maxVelocity:
+                    self.velocityDict[item] = self.maxVelocity
+                self.velocityDict[item] = -self.velocityDict[item]
 
             self.motors_queue.put(self.velocityDict)
             commandInfo(self.state)
