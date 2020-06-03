932edbcfc889fddeb1de63f51061f906d697533c
allen <2458490107@qq.com>
2020-06-03 18:14:25 +0800

    Revert "added stop then to feed"
    This reverts commit 1ac21e0383cbe5f85935d4dcd3dc0e6e5b479b35.

--- chassis.py
+++ chassis.py
@@ -51,28 +51,7 @@ class Controller(object):
     '''
 
     def __init__(self):
-<<<<<<< Updated upstream
         self.velocityDict = {}
-=======
-        self.velocityDict = {
-            'flWheel': 0,
-            'frWheel': 0,
-            'rlWheel': 0,
-            'rrWheel': 0,
-            'Shoulder': 0,
-            'Elbow': 0,
-            'Wrist': 0,
-        }
-        self.state = ''
-        self.feed_action = False
-        # parameters
-        self.maxVelocity = 45
-        self.defaultVelocity = 45
-        self.steer_coefficient = 0.5
-        self.feed_time = 0.3  # in seconds
-        self.flag = 0
-        self.timerecord = 0
->>>>>>> Stashed changes
         info('Chassis initialed')
 
     def set_queue(self, command_queue, motors_queue):
@@ -98,7 +77,6 @@ class Controller(object):
             self.velocityDict[motor] = 0
 
         if command:
-<<<<<<< Updated upstream
             if command == 'Move forward':
                 self.state = 'Moving forward'
                 self.velocityDict['wheel1'] = -10
@@ -126,66 +104,6 @@ class Controller(object):
                 self.velocityDict['wheel2'] = 10
                 self.velocityDict['wheel3'] = -10
                 self.velocityDict['wheel4'] = -10
-=======
-            if command == 'Feed' or self.feed_action or (self.flag > 0):
-                self.state = 'Feeding'
-                shoulder_vel = 5
-                elbow_vel = 0
-                wrist_vel = 8
-
-                if self.flag == 0:
-                    self.timerecord = time
-                    self.flag = 1
-                if time - self.timerecord > 0.3:
-                    self.flag = 2
-
-                if (self.feed_action is False) and (self.flag == 2):
-                    self.velocityDict['flWheel'] = 0
-                    self.velocityDict['rlWheel'] = 0
-                    self.velocityDict['rrWheel'] = 0
-                    self.velocityDict['frWheel'] = 0
-                    self.velocityDict['Shoulder'] = shoulder_vel
-                    self.velocityDict['Elbow'] = elbow_vel
-                    self.velocityDict['Wrist'] = -wrist_vel
-                    self.feed_action = True
-                    self.feed_start = time
-                    commandInfo(self.state)
-                elif(self.flag == 2):
-                    if time - self.feed_start < self.feed_time:
-                        pass
-                    elif time - self.feed_start < self.feed_time * 3:
-                        self.velocityDict['Shoulder'] = 0
-                        self.velocityDict['Elbow'] = 0
-                        self.velocityDict['Wrist'] = 0
-                    elif time - self.feed_start < self.feed_time * 5:
-                        self.velocityDict['Shoulder'] = -shoulder_vel / 1.8
-                        self.velocityDict['Elbow'] = -elbow_vel / 1.8
-                        self.velocityDict['Wrist'] = wrist_vel / 1.8
-                    else:
-                        self.velocityDict['Shoulder'] = 0
-                        self.velocityDict['Elbow'] = 0
-                        self.velocityDict['Wrist'] = 0
-                        commandInfo('Feeder recovered')
-                        self.feed_action = False
-                        self.flag = 0
-
-            # wheel
-            if command[:4] == 'Turn' and (self.feed_action is False):
-                steer = float(command[4:]) * self.steer_coefficient
-                self.state = 'Steering speed: ' + str(steer)
-                self.velocityDict['flWheel'] = self.defaultVelocity + steer
-                self.velocityDict['rlWheel'] = self.defaultVelocity + steer
-                self.velocityDict['rrWheel'] = self.defaultVelocity - steer
-                self.velocityDict['frWheel'] = self.defaultVelocity - steer
-                # wheel motor speed limitation and reverse
-                for motor in self.velocityDict:
-                    if 'Wheel' in motor:
-                        if self.velocityDict[motor] < -self.maxVelocity:
-                            self.velocityDict[motor] = -self.maxVelocity
-                        if self.velocityDict[motor] > self.maxVelocity:
-                            self.velocityDict[motor] = self.maxVelocity
-                        self.velocityDict[motor] = -self.velocityDict[motor]
->>>>>>> Stashed changes
 
             if command == 'Stop':
                 self.state = 'Stopped'
